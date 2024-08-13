5636d6ba8d9ee38bc4a47c4e1f08ed4b84692ebe
nihalcool123 <nihalmah123@gmail.com>
2024-08-13 19:44:16 +0530

    stepper component finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import Stepper from './components/Stepper'
+
+const steps = [
+  {
+    label: 'personal',
+    content: <div>personal</div>
+  },
+  {
+    label: 'account',
+    content: <div>account</div>
+  },
+  {
+    label: 'payment',
+    content: <div>payment</div>
+  },
+  {
+    label: 'confirmation',
+    content: <div>confirmation</div>
+  },
+  {
+    label: 'review',
+    content: <div>review</div>
+  }
+
+]
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Stepper steps={steps} />
     </>
   )
 }
